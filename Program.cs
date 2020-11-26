0b3d3c6c9f25d1260d5b2805a384e7490393e86e
John Livermore <jlivermore@bigwavesoftware.com>
2020-11-26 07:38:00 -0600

    1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GemBox.Spreadsheet;
+
+namespace GemBoxIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+            var excel = new ExcelFile();
+            var worksheet = excel.Worksheets.Add("Default");
+
+            worksheet.Cells[0, 0].SetValue("1234");
+            worksheet.Cells[1, 0].SetValue("5678");
+
+            worksheet.Cells[0, 1].SetValue(DateTime.Now);
+
+            worksheet.IgnoredErrors.Add("A:A", IgnoredErrorTypes.NumberStoredAsText);
+
+            excel.Save(@"c:\temp\gemboxExcelTest.xlsx", SaveOptions.XlsxDefault);
+        }
+    }
+}
